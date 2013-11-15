5d782aa9d1fd43677b9f17f4a9a7d00688cba232
Gabriel Luong <gabriel.luong@gmail.com>
2013-11-15 01:58:50 -0500

    Updated patterns

--- P1patterns.py
+++ P1patterns.py
@@ -2,20 +2,25 @@
 # 996268275
 import re
 
-##### Hearst's patterns for discovering hyponyms
+##### Regular expression of Hearst's patterns for discovering hyponyms
 
-### Components to Hearst's patterns
+### Regular expression components to Hearst's patterns
 
-# Return regular expression to extract the hyponym NP
+# Returns a regular expression to extract the hyponym NP from the given query
 def re_hyponym(query):
-    res = '(?P<hyponym>' + query + ')'
+    res = '(?P<hyponym>' + query + ')\s*'
+    return res
+
+# Returns a regular expression to extract the hypernym NP from the given query
+def re_hypernym(query):
+    res = '(?P<hypernym>' + query + ')\s*'
     return res
 
 # Regular expression to extract NP from the tree
 re_np = '\(NP[^)]*\)'
 
-# Regular expression to extract the hypernym NP
-re_hypernym = '(?P<hypernym>' + re_np + ')\s*'
+# Regular express to extract the hypernym NP from 'other NP'
+re_np_other = '\(NP\s+other/\w+\s+' + re_hypernym('[^)]*') + '\)'
 
 # Regular expression to match and not capture the commas
 re_comma = '(?:,/,\s+)'
@@ -23,7 +28,7 @@ re_comma = '(?:,/,\s+)'
 re_comma_optional =  re_comma + '?'
 
 # Regular expression to match and not capture {and|or}
-re_and_or = '(?:and/\S+|or/\S+)?'
+re_and_or = '(?:and/\w+|or/\w+)?'
 
 # Regular expression for NP_0{, NP_1, ...,}
 re_np_comma = re_np + '\s*((' + re_comma + re_np + '\s+)*)?'
@@ -33,45 +38,34 @@ re_np_and_or = re_np + '\s*((' + re_comma + re_np + '\s+)*' + \
     re_comma_optional + re_and_or + '\s+' + re_np + ')?'
 
 
-### Hearst's Patterns
+### Regular expression of Hearst's patterns
 
 # Pattern 1: NP_0{,} such as NP_1{, NP_2, ..., {and|or} NP_j}
-# \(NP[^)]*\)\s*(?:,/,\s+)?such/\S+\s+as/\S+\s+\(NP[^)]*\)\s*(((?:,/,\s+)\(NP[^)]*\)\s+)*(?:,/,\s+)?(?:and/\S+|or/\S+)?\s+\(NP[^)]*\))?
-# (?P<hypernym>\(NP[^)]*\))\s*(?:,/,\s+)?such/\S+\s+as/\S+\s+(?P<hyponym>\(NP[^)]*\)\s*(((?:,/,\s+)\(NP[^)]*\)\s+)*(?:,/,\s+)?(?:and/\S+|or/\S+)?\s+\(NP[^)]*\))?)
-re_hypernym_such_as_hyponym = re_hypernym + re_comma_optional + \
-    'such/\S+\s+as/\S+\s+' + re_hyponym(re_np_and_or)
+re_hypernym_such_as_hyponym = re_hypernym(re_np) + re_comma_optional + \
+    'such/\w+\s+as/\w+\s+' + re_hyponym(re_np_and_or)
 pattern1 = re.compile(re_hypernym_such_as_hyponym, flags=re.IGNORECASE)
 
 # Pattern 2: such NP_0 as NP_1{,NP_2,..., {and|or}NP_j}
-# such/\S+\s+\(NP[^)]*\)\s*as/\S+\s+\(NP[^)]*\)\s*(((?:,/,\s+)\(NP[^)]*\)\s+)*(?:,/,\s+)?(?:and/\S+|or/\S+)?\s+\(NP[^)]*\))?
-# such/\S+\s+(?P<hypernym>\(NP[^)]*\))\s*as/\S+\s+(?P<hyponym>\(NP[^)]*\)\s*(((?:,/,\s+)\(NP[^)]*\)\s+)*(?:,/,\s+)?(?:and/\S+|or/\S+)?\s+\(NP[^)]*\))?)
-re_such_hypernym_as_hyponym = 'such/\S+\s+' + re_hypernym + 'as/\S+\s+' + \
+re_such_hypernym_as_hyponym = 'such/\w+\s+' + re_hypernym(re_np) + 'as/\w+\s+' + \
     re_hyponym(re_np_and_or)
 pattern2 = re.compile(re_such_hypernym_as_hyponym, flags=re.IGNORECASE)
 
 # Pattern 3: NP_0{, NP_1, ...,} {and|or} other NP_j
-# \(NP[^)]*\)\s*(((?:,/,\s+)\(NP[^)]*\)\s+)*)?\s*(?:and/\S+|or/\S+)?\s*other/\S+\s+\(NP[^)]*\)\s*
-# (?P<hyponym>\(NP[^)]*\)\s*(((?:,/,\s+)\(NP[^)]*\)\s+)*)?)\s*(?:and/\S+|or/\S+)?\s*other/\S+\s+(?P<hypernym>\(NP[^)]*\))\s*
-re_hyponym_other_hypernym = re_hyponym(re_np_comma) + '\s*' + re_and_or + \
-    '\s*other/\S+\s+' + re_hypernym
+re_hyponym_other_hypernym = re_hyponym(re_np_comma) + re_and_or + \
+    '\s*' + re_np_other
 pattern3 = re.compile(re_hyponym_other_hypernym, flags=re.IGNORECASE)
 
 # Pattern 4: NP_0{,} including NP_1{, NP_2, ..., {and|or} NP_j}
-# \(NP[^)]*\)\s*(?:,/,\s+)?\s*including/\S+\s+\(NP[^)]*\)\s*(((?:,/,\s+)\(NP[^)]*\)\s+)*(?:,/,\s+)?(?:and/\S+|or/\S+)?\s+\(NP[^)]*\))?
-# (?P<hypernym>\(NP[^)]*\))\s*(?:,/,\s+)?\s*including/\S+\s+(?P<hyponym>\(NP[^)]*\)\s*(((?:,/,\s+)\(NP[^)]*\)\s+)*(?:,/,\s+)?(?:and/\S+|or/\S+)?\s+\(NP[^)]*\))?)
-re_hypernym_including_hyponym = re_hypernym + re_comma_optional + \
-    '\s*including/\S+\s+' + re_hyponym(re_np_and_or)
+re_hypernym_including_hyponym = re_hypernym(re_np) + re_comma_optional + \
+    '\s*including/\w+\s+' + re_hyponym(re_np_and_or)
 pattern4 = re.compile(re_hypernym_including_hyponym, flags=re.IGNORECASE)
 
 # Pattern 5: NP_0{,} especially NP_1{, NP_2, ..., {and|or}}
-# \(NP[^)]*\)\s*(?:,/,\s+)?\s*especially/\S+\s+\(NP[^)]*\)\s*(((?:,/,\s+)\(NP[^)]*\)\s+)*(?:,/,\s+)?(?:and/\S+|or/\S+)?\s+\(NP[^)]*\))?
-# (?P<hypernym>\(NP[^)]*\))\s*(?:,/,\s+)?\s*especially/\S+\s+(?P<hyponym>\(NP[^)]*\)\s*(((?:,/,\s+)\(NP[^)]*\)\s+)*(?:,/,\s+)?(?:and/\S+|or/\S+)?\s+\(NP[^)]*\))?)
-re_hypernym_especially_hyponym = re_hypernym + re_comma_optional + \
-    '\s*especially/\S+\s+' + re_hyponym(re_np_and_or)
+re_hypernym_especially_hyponym = re_hypernym(re_np) + re_comma_optional + \
+    '\s*especially/\w+\s+' + re_hyponym(re_np_and_or)
 pattern5 = re.compile(re_hypernym_especially_hyponym, flags=re.IGNORECASE)
 
 patterns = [pattern1, pattern2, pattern3, pattern4, pattern5]
-
 
 if __name__ == "__main__":
     print re_hypernym_such_as_hyponym
@@ -79,3 +73,22 @@ if __name__ == "__main__":
     print re_hyponym_other_hypernym
     print re_hypernym_including_hyponym
     print re_hypernym_especially_hyponym
+    print
+    # Sample test to extract the hyponym and hypernym from an example sentence
+    # for pattern 1
+    s = "(S  With/IN  current/JJ  manufactured/VBN  (NP drainage/NN systems/NNS)  ,/,  (NP pollutants/NNS)  such/JJ  as/IN  (NP grease/NN)  ,/,  (NP fertilizers/NNS)  and/CC  (NP insect/NN pesticides/NNS)  make/VBP  their/PP$  (NP way/NN)  into/IN  (NP streams/NNS)  and/CC  (NP rivers/NNS)  ./SENT)"
+    other = "(NP Mr./NP Everest/NP)  and/CC  (NP other/JJ small-business/NN owners/NNS)"
+    matches = pattern1.search(s)
+    hypernym = matches.group('hypernym')
+    hyponym = matches.group('hyponym')
+    matches = re.findall('\(NP\s+[^)]*\)', hyponym, flags=re.IGNORECASE)
+    print hypernym
+    print matches
+    print
+    matches = pattern3.search(other)
+    hypernym = matches.group('hypernym')
+    hyponym = matches.group('hyponym')
+    print hyponym
+    matches = re.findall('(\S+)/\w+', "(NP Mr/NP Everest/NP)", flags=re.IGNORECASE)
+    print hypernym
+    print matches
